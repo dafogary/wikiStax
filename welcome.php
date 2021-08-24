65b08546821ed722e5efb15224f3036bef34a14a
harryc <harry@cadby.co.uk>
2021-08-24 15:12:22 +0100

    updates
    fixed issue with register.php not updating database,
    updated table formatting and welcome buttons

--- welcome.php
+++ welcome.php
@@ -19,9 +19,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <?php include_once("menu.php"); ?>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to the MediaWiki Management Interface.</h1>
     <p>
-        <a href="register.php" class="btn btn-dark">Create User Account</a>
-        <a href="reset-password.php" class="btn btn-warning ml-3">Change Your Password</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+        <a href="createwiki.php" class="btn btn-dark">Create wiki</a>
+        <a href="#" class="btn btn-warning ml-3">Spare</a>
+        <a href="removewiki.php" class="btn btn-danger ml-3">Remove Wiki</a>
     </p>
 </body>
 </html>
