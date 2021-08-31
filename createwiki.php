0482a931a55d51cd5a10b5adc7915aa4b88e96a8
harryc <harry@cadby.co.uk>
2021-08-31 16:33:57 +0100

    Fixed redirection on createwiki.php

--- createwiki.php
+++ createwiki.php
@@ -181,7 +181,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			
 			// Attempt to execute the prepared statement
 			if(mysqli_stmt_execute($stmt)){
-				header("locationL welcome.php"); // Redirect back to welcome page once wiki creation is complete.
+				header("location: welcome.php"); // Redirect back to welcome page once wiki creation is complete.
 			} else{
 				echo "Oops! The wiki wasn't added to the database.";
 			}
